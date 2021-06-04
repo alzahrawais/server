33b4bb145a3324cea1508d5485db751463bd19b5
alzahrawais <magaries1728@gmail.com>
2021-06-05 01:50:35 +0500

    updated server with embeded database

--- app/api/model/empModel.js
+++ app/api/model/empModel.js
@@ -1,14 +1,81 @@
 const mongoose = require('mongoose')
 mongoose.pluralize(null)
 
+ //ObjectId = mongoose.Schema.ObjectId;
 
 var Schema = mongoose.Schema;
 
 var empSchema = new Schema({
-     name:{type:String,required:true,unique:true},
-     fname:{type:String,required:true,unique:true},
-     model_number:String
+     lawn:[lawnSchema],
+     chantelle:[chantelleSchema],
+     swiss:[swissSchema],
+     fuchsia:[fuchsiaSchema]
+    
      
+});
+var lawnSchema = new Schema({
+     Size:String,
+     pieces:Number,
+     fabric_type:String,
+     prices:Number
+});
+var chantelleSchema = new Schema({
+     Size:String,
+     pieces:Number,
+     fabric_type:String,
+     prices:Number
+});
+var swissSchema = new Schema({
+     Size:String,
+     pieces:Number,
+     fabric_type:String,
+     prices:Number
 })
-var employee  = mongoose.model('gg',empSchema)
+var fuchsiaSchema = new Schema({
+     Size:String,
+     pieces:Number,
+     fabric_type:String,
+     prices:Number
+})
+
+var employee  = mongoose.model('collections',empSchema)
 module.exports = employee
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/POST schema
+
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+});
+
+var Post = mongoose.model("Post", postSchema);
+
+//USER schema
+
+var userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    posts: [
+            {
+                 type: mongoose.Schema.Types.ObjectID,   
+                 ref: "Post"
+            }
+         ]
+});
+
+var User = mongoose.model("User", userSchema);
--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/fs23', {useNewUrlParser: true, useUnifiedTopology: true},(err)=>{
+mongoose.connect('mongodb://localhost:27017/Baroque', {useNewUrlParser: true, useUnifiedTopology: true},(err)=>{
 if(!err)
 {
     console.log("Connection succeeded")
